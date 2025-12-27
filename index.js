3d66e4221f8fa845678c74a69b84e10f5d4c808f
Cameron Brown <jupiter49777@gmail.com>
2025-12-27 20:43:44 +0200

    feat: Add a function to handle creating a player object

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const Gameboard = (() => {
   const gameboardArr = ['', '', '', '', '', '', '', '', ''];
   return { gameboardArr };
 })();
+
+function createPlayer(name, marker) {
+  return { name, marker };
+}
