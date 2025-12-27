7447ead670f9d59b36dfa2b8c1914bcd4c72a6a2
Cameron Brown <jupiter49777@gmail.com>
2025-12-27 21:10:24 +0200

    feat: Add a function to handle wins and ties

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Gameboard = (() => {
-  const gameboardArr = ['', '', '', '', '', '', '', '', ''];
+  const gameboardArr = ['o', 'o', 'o', 'o', 'x', 'x', 'x', 'o', 'o'];
   return { gameboardArr };
 })();
 
@@ -9,4 +9,30 @@ function createPlayer(name, marker) {
 
 const playGame = (() => {
   const gameboard = Gameboard.gameboardArr;
+
+  const checkWin = () => {
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (const [a, b, c] of winningCombos) {
+      if (
+        gameboard[a] &&
+        gameboard[a] === gameboard[b] &&
+        gameboard[a] === gameboard[c]
+      ) {
+        console.log(`${gameboard[a]} wins`);
+        return;
+      }
+    }
+    if (gameboard.every((square) => square)) console.log('tie');
+  };
+  return { checkWin };
 })();
+playGame.checkWin();
